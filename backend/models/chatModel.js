ddba7d8a8e893f1f8c7903c5dbbe518b418ab47f
Hritik Chaudhary <hritikchoudhary943@gmail.com>
2025-01-14 04:25:36 +0530

    Created Chat Routes and Controllers for 1-1 chat, group chat and fixed an export syntax

--- backend/models/chatModel.js
+++ backend/models/chatModel.js
@@ -3,7 +3,7 @@ const mongoose = require("mongoose");
 const chatModel = mongoose.Schema({
     chatName: { type: String, trim: true },
     isGroupChat: { type: Boolean, default: false },
-    users: { type: mongoose.Schema.Types.ObjectId, ref: "User" },
+    users: [{ type: mongoose.Schema.Types.ObjectId, ref: "User" }],
     latestMessage: { type: mongoose.Schema.Types.ObjectId, ref: "Message" },
     groupAdmin: { type: mongoose.Schema.Types.ObjectId, ref: "User" },
 }, {
@@ -11,4 +11,5 @@ const chatModel = mongoose.Schema({
 });
 
 const Chat = mongoose.model("Chat", chatModel);
-module.exports(Chat);
+
+module.exports = Chat;
