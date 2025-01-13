799ca68660fcbd8bdf44edd86d08a60e944cf60e
Hritik Chaudhary <hritikchoudhary943@gmail.com>
2025-01-14 04:55:00 +0530

    Created more API endpoints, for renaming a group chat, adding and removing a user from the group

--- backend/routes/chatRoutes.js
+++ backend/routes/chatRoutes.js
@@ -8,7 +8,7 @@ router.route('/').post(protect, accessChat);
 router.route('/').get(protect, fetchChats);
 router.route('/group').post(protect, createGroupChat);
 router.route('/rename').put(protect, renameGroup);
-router.route('/group-remove').put(protect, removeFromGroup);
 router.route('/group-add').put(protect, addToGroup);
+router.route('/group-remove').put(protect, removeFromGroup);
 
 module.exports = router;
